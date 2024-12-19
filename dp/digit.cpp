9125e98309d3acba29590f9fd432fe69332c0bb2
Wassim Al Haraki <wassimalharaki@gmail.com>
2024-12-19 16:15:19 +0200

    added thomas's algo to solve tridiagonal matrices

--- dp/digit.cpp
+++ dp/digit.cpp
@@ -1,16 +1,12 @@
-int dp[20][2][2];
+int dp[20][2];
 
-int f(string& s, int n, bool lz = 1, bool t = 1) {
-    if (dp[n][lz][t] != -1)
-        return dp[n][lz][t];
-    int& ans = dp[n][lz][t] = 0;
+int f(string& s, int i = 0, bool t = 1) {
+    if (dp[n][t] != -1)
+        return dp[n][t];
+    int& ans = dp[n][t] = 0;
+    int r = t ? s[i] - '0' : 9;
 
-    int ans = 0;
-    for (int i = 0; i <= r; i++)
-        if (i == 0)
-            ans += f(s, n - 1, x, lz, 0);
-        else
-            ans += f(s, n - 1, x - 1, 0, 0);
-
+    for (int j = 0; j <= r; j++)
+        ans += f(s, i + 1, x - 1, t and i == r);
     return ans;
 }
---
+++ math/thomas.cpp
@@ -0,0 +1,15 @@
+// O(n * log(b[i]))
+// a[i] * f(x - 1) + b[i] * f(x) + c[i] * f(x + 1) = d[i]
+vector<int> thomas(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
+    int n = a.size();
+    vector<int> x(n);
+    for (int i = 1; i < n; i++) {
+        int w = a[i] * modinv(b[i - 1]) % mod;
+        b[i] = (b[i] - w * c[i - 1] % mod + mod) % mod;
+        d[i] = (d[i] - w * d[i - 1] % mod + mod) % mod;
+    }
+    x[n - 1] = d[n - 1] * modinv(b[n - 1]) % mod;
+    for (int i = n - 2; i >= 0; i--)
+        x[i] = (d[i] - c[i] * x[i + 1] % mod + mod) * modinv(b[i]) % mod;
+    return x;
+}
